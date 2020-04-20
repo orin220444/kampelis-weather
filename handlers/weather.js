7a9ca1e35b44e13be3634f51b9747192276a96ac
Orin Serimon <orin220444@protonmail.com>
2020-04-20 10:03:43 +0000

    bug fixes

--- handlers/weather.js
+++ handlers/weather.js
@@ -1,8 +1,15 @@
 const weather = require('openweather-apis')
+const bot = require('../bot')
 module.exports = async (ctx) => {
 weather.setLang('ru');
- weather.setCity('Moscow');
 weather.setAPPID(process.env.OWM_KEY);
+ctx.reply('В каком городе вы находитесь?')
+await getCity(ctx)
+console.log(city)
+weather.setCity(city);
+getWeather(city, ctx)
+
+async function getWeather(city, ctx){
 await weather.getAllWeather(function(err, JSONObj){
        console.log(JSONObj) //sendWeather(JSONObj)
 ctx.reply(`Погода в ${JSONObj.name}
@@ -12,4 +19,12 @@ ctx.reply(`Погода в ${JSONObj.name}
 Давление: ${JSONObj.main.pressure} мм рт.
 Влажность: ${JSONObj.main.humidity}%`)
     });
+}
+}
+async function getCity(ctx){
+bot.on('message', async (ctx) => {
+const city = await ctx.message.text
+console.log(city)
+return city
+})
 }
