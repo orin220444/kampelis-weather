8c75b8851d6b884ddc50934bc8eea7c63daab700
Orin Serimon <orin220444@protonmail.com>
2020-04-21 05:13:41 +0000

    move bot initialization to bot.js

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 require('dotenv').config({path: './.env'});
-//const Telegraf = require('telegraf');
-const bot = require('./bot')
-//const bot = new Telegraf(process.env.BOT_TOKEN);
-const {handleWeather} = require('./handlers')
-bot.command('weather', handleWeather)
-console.log(handleWeather)
-bot.launch()
+const bot = require('./bot');
+const {handleWeather} = require('./handlers');
+bot.command('weather', handleWeather);
+console.log(handleWeather);
+bot.launch();
